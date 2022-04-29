00aeab43d293920f4d490b516693242b35ad2f7d
Eric Harvey <swimleft@gmail.com>
2022-04-29 12:23:41 -0700

    connecting databased local and cloud

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const noobinRoutes = require('./routes/noobin')
 noobinRoutes(app)
 
 
-if(process.env.NODE_ENV === 'production'){
+if(process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'hub_dev'){
   app.use(express.static('client/build'))
   app.get('/*',(req,res) => {
   res.sendFile(path.resolve(__dirname,"client","build","index.html"))        
